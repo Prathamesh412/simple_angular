6da5f4d611a75c8b5a898de19afcf714c62e8fb1
Prathamesh <prathprabhu@gmail.com>
2015-12-04 16:08:03 +0530

    First commit

--- app.js
+++ app.js
@@ -6,18 +6,21 @@
         templateUrl: 'list.html',
         controller: 'Ctrl'
       })
-      .when('/edit',{
+      .when('/edit/:id',{
         templateUrl: 'edit.html',
         controller: 'Edit'
       })
+      .when('/add',{
+        templateUrl: 'add.html',
+        controller: 'Add'
+      })
+     .when('/remove',{
+        templateUrl: 'remove.html',
+        controller: 'Remove'
+      })
     })
-    .controller('Ctrl',['$scope', function(scope){
-        console.log(scope);
-            scope.addStatus = true;
-            scope.name = "Rafal";
-            scope.age = "28";
-            scope.persons=['Tom', 'Jerry']; 
-            scope.people =[
+    .run(function($rootScope){
+          $rootScope.people =[
               {
                 name: "Sobin", 
                 age: 23
@@ -39,25 +42,42 @@
                 age: 13
               }
             ];
-          scope.add = function(){
-            scope.people.push(scope.person);
-            scope.person ={};
-          };
-          scope.remove = function(index){
-             scope.people.splice(index, 1);
-          };
-          scope.edit = function(index){
-            scope.addStatus = false;
-            scope.person = scope.people[index];
-          };
+})
+    .controller('Ctrl',['$scope','$rootScope',function(scope,$rootScope){
+        console.log(scope);
+            scope.addStatus = true;
+            scope.name = "Rafal";
+            scope.age = "28";
+            scope.persons=['Tom', 'Jerry']; 
+            
+//          scope.remove = function(index){
+//             scope.people.splice(index, 1);
+//          };
+       
           scope.save = function(){
             scope.addStatus = true;
             scope.person = [];
           };
-          
-          
+       
+    }])
+    .controller('Edit',['$scope','$rootScope','$routeParams',function(scope,$rootScope,$routeParams){
+        scope.edit = function(index){
+            scope.addStatus = false;
+            scope.person = $rootScope.people[$routeParams.id];
+          };   
+     
+    }])
+    .controller('Add',['$scope','$rootScope',function($scope,$rootScope){
+         $scope.add = function(){
+            $rootScope.people.push($scope.person);
+            $scope.person ={};
+          };
+    }])
+    
+    .controller('Remove',['$scope','$rootScope',function($scope,$rootScope){
+      $scope.people=$rootScope.people    
+      $scope.remove = function(index){
+             $rootScope.people.splice(index, 1);
+          };
       
     }])
-   .controller('Edit',['$scope', function(scope){
-
-    }]);
